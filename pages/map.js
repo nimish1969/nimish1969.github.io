246b04209b3812662cbdc8cc252e976ef4e25ea0
Nimish Shah <shahnimish.1969@gmail.com>
2025-02-02 16:20:44 +0530

    Update map.js

--- pages/map.js
+++ pages/map.js
@@ -1,57 +1,57 @@
-// import React from 'react';
-// import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import React from 'react';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
-// const containerStyle = {
-//   width: '400px',
-//   height: '400px',
-// }
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+}
 
-// const center = {
-//   lat: 22.3222406,
-//   lng: 73.0906853,
-// }
+const center = {
+  lat: 22.3222406,
+  lng: 73.0906853,
+}
 
-// // Define the marker position
-// const markerPosition = {
-//   lat: 22.3222406,  // Latitude (same as center for simplicity)
-//   lng: 73.0906853, // Longitude (same as center for simplicity)
-// };
+// Define the marker position
+const markerPosition = {
+  lat: 22.3222406,  // Latitude (same as center for simplicity)
+  lng: 73.0906853, // Longitude (same as center for simplicity)
+};
 
-// function MapComponent() {
+function MapComponent() {
 
-//   const { isLoaded } = useJsApiLoader({
-//     id: 'google-map-script',
-//     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-//   })
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  })
 
-//   const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null)
 
-//   const onLoad = React.useCallback(function callback(map) {
-//     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-//     const bounds = new window.google.maps.LatLngBounds(center)
-//     map.fitBounds(bounds)
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center)
+    map.fitBounds(bounds)
 
-//     setMap(map)
-//   }, [])
+    setMap(map)
+  }, [])
 
-//   const onUnmount = React.useCallback(function callback(map) {
-//     setMap(null)
-//   }, [])
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
 
-//   return isLoaded ? (
-//     <GoogleMap
-//       mapContainerStyle={containerStyle}
-//       center={center}
-//       zoom={10}
-//       onLoad={onLoad}
-//       onUnmount={onUnmount}
-//     >
-//      {/* Add a marker to the map */}
-//      <Marker position={markerPosition} />
-//     </GoogleMap>
-//   ) : (
-//     <></>
-//   )
-// }
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+     {/* Add a marker to the map */}
+     <Marker position={markerPosition} />
+    </GoogleMap>
+  ) : (
+    <></>
+  )
+}
 
-// export default React.memo(MapComponent);
+export default React.memo(MapComponent);
