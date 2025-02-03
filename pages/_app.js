07aa23b8b8d8bad61c0bd6fc1cae48d05fdf14ef
Nimish Shah <shahnimish.1969@gmail.com>
2025-02-04 00:03:09 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@
 
 // pages/_app.js
 import '../styles/global.css';  // Import Tailwind CSS or your custom global styles
+import '../styles/tailwind.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
